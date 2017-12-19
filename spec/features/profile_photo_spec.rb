a7bfb8e7ba3cd42b2c15388ba2215b636aac7212
clb840212 <christina.l.beauregard@outlook.com>
2017-12-19 11:47:08 -0500

    added profile pic

---
+++ spec/features/profile_photo_spec.rb
@@ -0,0 +1,20 @@
+# Add pic tests
+require 'rails_helper'
+
+feature 'upload profile photo' do
+  let!(:user) { FactoryBot.create(:user) }
+
+
+  scenario 'user uploads profile picture' do
+    visit user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: 'Password'
+    click_button 'Sign In'
+    visit edit_user_registration_path
+    fill_in 'Current password', with: 'Password'
+    attach_file 'Profile Photo', "#{Rails.root}/spec/support/images/default_pic.jpeg"
+    click_button 'Update'
+
+    expect(page).to have_content("Your account has been updated successfully.")
+  end
+end
