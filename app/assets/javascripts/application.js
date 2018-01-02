f988ad790604675912da3b9ee0eec724d759c076
clb840212 <christina.l.beauregard@outlook.com>
2018-01-02 17:45:18 -0500

    added bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
-
+//= require jquery
+//= require bootstrap-sprockets
 //= require_tree .
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180102205324) do
+ActiveRecord::Schema.define(version: 20180102220220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20180102205324) do
     t.string "mood"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "workout_id"
   end
 
 end
